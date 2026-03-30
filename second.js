56032786ca73b99bd868e85ca7b0f36b99b08eac
NikitaKrilovets <nikita.krilovets@gimn14.lutsk.ua>
2026-03-30 19:31:22 +0300

    change

--- second.js
+++ second.js
@@ -2,6 +2,7 @@
 let a = document.getElementById("add");
 let b = document.getElementById("list");
 let c = document.getElementById("input");
+let sortBtn = document.getElementById("sort");
 
 a.addEventListener("click", function() {
     let text = c.value;
@@ -14,4 +15,8 @@ a.addEventListener("click", function() {
         li.remove();
         });
     }
+});
+
+sortBtn.addEventListener("click", () => {
+    b.innerHTML = Array.from(b.children).sort((a, b) => a.textContent.localeCompare(b.textContent)).map(li => li.outerHTML).join("");
 });
